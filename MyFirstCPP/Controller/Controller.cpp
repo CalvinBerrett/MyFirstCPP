8744bbb811cbad81bf8f1a2e7f5ebdc8fd124b51
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2018-01-23 12:46:02 -0700

    Created Controller Header

---
+++ MyFirstCPP/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  MyFirstCPP
+//
+//  Created by Berrett, Calvin on 1/23/18.
+//  Copyright © 2018 Berrett, Calvin. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ MyFirstCPP/Controller/Controller.hpp
@@ -0,0 +1,22 @@
+//
+//  Controller.hpp
+//  MyFirstCPP
+//
+//  Created by Berrett, Calvin on 1/23/18.
+//  Copyright © 2018 Berrett, Calvin. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+
+class Controller
+{
+public:
+    Controller();
+    void start();
+    
+};
+
+#endif /* Controller_hpp */
