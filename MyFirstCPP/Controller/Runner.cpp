b3fd6ae946ab2a115cf7d9fa16d23289e6d17861
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2018-01-23 12:38:15 -0700

    made oldMain method. And made my code do nothing

---
+++ MyFirstCPP/Controller/Runner.cpp
@@ -0,0 +1,15 @@
+//
+//  Runner.cpp
+//  MyFirstCPP
+//
+//  Created by Berrett, Calvin on 1/23/18.
+//  Copyright © 2018 Berrett, Calvin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
