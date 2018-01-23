130db2efc951e0c9921245e5e3092595186b6de0
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2018-01-23 12:12:17 -0700

    Initial Commit

---
+++ MyFirstCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyFirstCPP
+//
+//  Created by Berrett, Calvin on 1/23/18.
+//  Copyright © 2018 Berrett, Calvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
